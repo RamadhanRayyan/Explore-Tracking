b52662dd985a92bc7210a7f368fd5f9a582cb0e6
RamadhanRayyan <rama.rayyan@nusabs.sch.id>
2026-09-25 11:29:47 +0700

    add /

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: "/Explore-Tracking/",
     plugins: [vue(), tailwindcss()],
     resolve: {
       alias: {
